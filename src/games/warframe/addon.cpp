ec3e5022637feb711a7c99807d5eba4003f23649
HBoyYT <hboyytbusiness@gmail.com>
2025-08-30 14:26:48 +0100

    feat(warframe): add auto detection for peak and paper white

--- src/games/warframe/addon.cpp
+++ src/games/warframe/addon.cpp
@@ -435,49 +435,50 @@
    renodx::utils::settings::UpdateSetting("TransitionFix", 0.f);
  }
  
+bool fired_on_init_swapchain = false;
+
 void OnInitSwapchain(reshade::api::swapchain* swapchain, bool resize) {
+  if (fired_on_init_swapchain) return;
+  fired_on_init_swapchain = true;
+
   auto peak = renodx::utils::swapchain::GetPeakNits(swapchain);
-  auto white_level = 203.f;
-  if (peak.has_value()) {
-    settings[1]->default_value = peak.value();
-  } else {
-    settings[1]->default_value = 1000.f;
+  if (!peak.has_value()) {
+    peak = 1000.f;
   }
-
-  settings[2]->default_value = renodx::utils::swapchain::ComputeReferenceWhite(settings[1]->default_value);
+  
+  settings[2]->default_value = peak.value();
+  settings[3]->default_value = renodx::utils::swapchain::ComputeReferenceWhite(peak.value());
 }
+bool initialized = false;
 
- bool initialized = false;
- 
- }  // namespace
+}  // namespace
  
  extern "C" __declspec(dllexport) constexpr const char* NAME = "RenoDX";
  extern "C" __declspec(dllexport) constexpr const char* DESCRIPTION = "RenoDX (Warframe)";
  
- BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
-   switch (fdw_reason) {
-     case DLL_PROCESS_ATTACH:
-       if (!reshade::register_addon(h_module)) return FALSE;
- 
-       if (!initialized) {
-         renodx::mods::swapchain::SetUseHDR10();     
-         renodx::mods::shader::expected_constant_buffer_space = 50;
-         renodx::mods::shader::expected_constant_buffer_index = 13;
-        // renodx::mods::shader::allow_multiple_push_constants = true;
+BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
+  switch (fdw_reason) {
+    case DLL_PROCESS_ATTACH:
+      if (!reshade::register_addon(h_module)) return FALSE;
 
-         initialized = true;
-       }
- 
-       break;
-     case DLL_PROCESS_DETACH:
-       reshade::unregister_addon(h_module);
-       break;
-   }
+      if (!initialized) {
+        renodx::mods::swapchain::SetUseHDR10();     
+        renodx::mods::shader::expected_constant_buffer_space = 50;
+        renodx::mods::shader::expected_constant_buffer_index = 13;
 
-   renodx::utils::settings::Use(fdw_reason, &settings, &OnPresetOff);
-   renodx::mods::swapchain::Use(fdw_reason, &shader_injection);
-   renodx::mods::shader::Use(fdw_reason, custom_shaders, &shader_injection);
- 
-   return TRUE;
- }
- 
+        initialized = true;
+      }
+
+      reshade::register_event<reshade::addon_event::init_swapchain>(OnInitSwapchain);  // peak nits detection
+      break;
+    case DLL_PROCESS_DETACH:
+      reshade::unregister_addon(h_module);
+      reshade::unregister_event<reshade::addon_event::init_swapchain>(OnInitSwapchain);  // peak nits detection
+      break;
+  }
+
+  renodx::utils::settings::Use(fdw_reason, &settings, &OnPresetOff);
+  renodx::mods::shader::Use(fdw_reason, custom_shaders, &shader_injection);
+
+  return TRUE;
+}
