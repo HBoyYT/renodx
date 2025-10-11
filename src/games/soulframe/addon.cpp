5724ac6e2aba39815bd19ace6adbf307de15bf4b
HBoyYT <hboyytbusiness@gmail.com>
2025-10-11 17:35:09 +0100

    fix(soulframe): fix incorrect description

--- src/games/soulframe/addon.cpp
+++ src/games/soulframe/addon.cpp
@@ -534,7 +534,7 @@ bool initialized = false;
 }  // namespace
  
  extern "C" __declspec(dllexport) constexpr const char* NAME = "RenoDX";
- extern "C" __declspec(dllexport) constexpr const char* DESCRIPTION = "RenoDX (Warframe)";
+ extern "C" __declspec(dllexport) constexpr const char* DESCRIPTION = "RenoDX (Soulframe)";
  
 BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
   switch (fdw_reason) {
